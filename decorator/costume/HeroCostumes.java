cffe9a0f27014f4ef3d2bdf80932f343f1c881a5
atakanaytan <aytan.atakan2@gmail.com>
2020-05-23 15:39:30 +0300

    Decorator pattern was added

---
+++ decorator/costume/HeroCostumes.java
@@ -0,0 +1,8 @@
+package decorator.costume;
+
+import decorator.hero.Hero;
+
+public abstract class HeroCostumes extends Hero {
+
+    public abstract String getDescription();
+}
---
+++ decorator/costume/SpeacialHealthBoostCostume.java
@@ -0,0 +1,32 @@
+package decorator.costume;
+
+import decorator.hero.Hero;
+
+public class SpeacialHealthBoostCostume extends HeroCostumes {
+
+    Hero hero;
+
+    public SpeacialHealthBoostCostume(Hero hero) {
+        this.hero = hero;
+    }
+
+    @Override
+    public String getDescription() {
+        return hero.getDescription() + " Equipped with special health booster costume";
+    }
+
+    @Override
+    public double attack() {
+        return hero.attack();
+    }
+
+    @Override
+    public double superAttack() {
+        return hero.superAttack();
+    }
+
+    @Override
+    public double getHealthInfo() {
+        return 200 + hero.getHealthInfo();
+    }
+}
